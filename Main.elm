364d09fcb1ed5a842c9c040c06d2ab4675c934a5
Jason Neylon <jason.neylon@gmail.com>
2017-04-11 16:27:31 +0200

    Grouping things into clusters

--- Main.elm
+++ Main.elm
@@ -1,11 +1,9 @@
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Random exposing (..)
+import Array exposing (..)
+import Maybe exposing (..)
 import Html exposing (..)
-
---- randomPoints : List(Int,Int)
---- randomPoints =
----     Tuple.first (Random.step (Random.list 10 <| Random.pair (int 1 100) (int 1 100)) (Random.initialSeed 10))
 
 main =
   Html.program
@@ -16,48 +14,101 @@ main =
     }
 
 
-
 -- UPDATE
 
-
-randomPoints = 
-  Random.list 10 (Random.pair (int 1 100) (int 1 100))
+type alias Point = { x:Int, y:Int }
 
 type Msg
-  = GeneratePoints
-  | DrawGraph (List(Int, Int)) 
+  = PickPoints (List Point)
+  | DrawGraph (List Point) 
 
--- update : Msg -> Model -> (Model, Cmd Msg)
+randomPoints : Generator (List(Point))
+randomPoints = 
+  Random.list 200 (Random.map (\(x, y) -> Point x y) (Random.pair (int 1 999) (int 1 499)))
+
+defaultPoint : Point
+defaultPoint =
+  Point 0 0
+
+pointByIndex : List Point -> Int -> Point
+pointByIndex points index = 
+  withDefault defaultPoint (Array.get index (Array.fromList points))
+
+randomElements : Int -> List Point -> Generator (List Point)
+randomElements k points =
+  let 
+    l = List.length points
+  in 
+    (Random.list k (Random.map (\i -> (pointByIndex points i)) (int 1 l)))
+
+distance : Point -> Point -> Float
+distance a b = 
+  sqrt (toFloat (((a.x - b.x) ^ 2) + ((a.y - b.y) ^ 2)))
+
+closestCentroid : Point -> List(Point) -> Point
+closestCentroid point centroids = 
+  let 
+    centroidsByDistance = List.map (\c -> let d = (distance point c) in {centroid=c, distance=d}) centroids
+  in
+    centroidsByDistance
+    |> List.sortBy .distance 
+    |> List.head
+    |> Maybe.withDefault {centroid=defaultPoint, distance=0}
+    |> .centroid
+
+groupIntoCentroidAndPoints : List { centroid : Point, point: Point } -> Point -> { centroid : Point, points : List Point }
+groupIntoCentroidAndPoints pointsAndClosestCentroid c =
+  let 
+    points = List.map .point (List.filter (\pc -> pc.centroid == c) pointsAndClosestCentroid)
+  in 
+    { centroid=c, points=points }
+
+groupIntoClusters : List Point -> List Point -> List Cluster
+groupIntoClusters points centroids =
+  let 
+    pointsAndClosestCentroid = List.map (\p -> { point=p, centroid=(closestCentroid p centroids)}) points
+  in
+    List.map (groupIntoCentroidAndPoints pointsAndClosestCentroid) centroids
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    GeneratePoints ->
-      (model, (Random.generate DrawGraph randomPoints))
-      --(model, Cmd.none)
-    DrawGraph points ->
-      (Model points, Cmd.none)
-
+    PickPoints points ->
+      ({model| points = points }, (Random.generate DrawGraph (randomElements model.k points)))
+    DrawGraph centroids ->
+      ({model | centroids = centroids, clusters = (groupIntoClusters model.points centroids) }, Cmd.none)
+      -- ({model | centroids = centroids }, Cmd.none)
 
 -- MODEL
 
-
-type alias Model =
-  { points : List(Int, Int)
+type alias Cluster = 
+  { centroid : Point
+  , points : List(Point)
   }
 
+type alias Model =
+  { points : List(Point)
+  , centroids : List(Point)
+  , clusters : List(Cluster)
+  , k : Int
+  }
 
 init : (Model, Cmd Msg)
 init =
-  (Model [], (Random.generate DrawGraph randomPoints))
+  (Model [] [] [] 4, (Random.generate PickPoints randomPoints))
 
 -- VIEW
 
 view model =
     svg
-    [version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95"]
-    (List.map (\p ->  circle [ cx (toString (Tuple.first p)), cy (toString (Tuple.second p)), r "1" ] []) model.points)
-                     
+    [version "1.1", x "0", y "0", viewBox "0 0 1000 500"]
+    (List.map (renderPoint model.centroids) model.points)
 
-
+renderPoint centroids p =
+  let 
+    color = if List.member p centroids then "red" else "blue"
+  in  
+    circle [ cx (toString p.x), cy (toString p.y), r "2", fill color ] []
 
 
 -- SUBSCRIPTIONS
